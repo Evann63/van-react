e300fb9947da34745b0684abcca348fccac89fd7
Evann63 <evanderjuventino@gmail.com>
2025-08-12 13:41:49 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return
-}
+    const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>KLIKK</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 600)}>
+          count is {count}
+        </button>
+      </div>
+    
+    </>
+  )
 
 export default App
